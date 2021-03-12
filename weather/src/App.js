36258135c5cf9f976a3831d111aa5e9fdde63d54
markobozic346 <55995892+markobozic346@users.noreply.github.com>
2021-03-12 22:46:56 +0100

    fixed bugs

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,33 +1,39 @@
-
-import './App.css';
-import React,{useEffect, useState} from 'react'
+import "./App.css";
+import React, { useEffect, useState } from "react";
 function App() {
-const [data, setData] = useState("");
-// call fetch data function on initialization
- useEffect(() => {
-  async function fetchData(){
-    let response = await fetch('https://api.openweathermap.org/data/2.5/forecast?q=smederevska%20palanka&units=metric&appid=d7c09da0824a3c72c99771435764c729');
-    let data = await response.json()
-    setData(data.list);
+
+  const [data, setData] = useState("");
+  const [change, setChange] = useState("");
+  const [cityName, setCityName] = useState(localStorage.hasOwnProperty('city') ? localStorage.getItem['city'] : 'Beograd');
+  
+  useEffect(() => {
+    async function fetchData() {
+      let response = await fetch(
+       `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.REACT_APP_API_KEY}`
+      );
+      let data = await response.json();
+      setData(data);
+    }
+    fetchData();
+
     
-  
+    
+  }, [cityName]);
+
+  const getCity = () => {
+    return 
   }
-  fetchData();
-   
- },[])
-  
 
+  const handleClick = () => {
+    localStorage['city'] = change;
+    
+  }
 
-  return (
-
-  
-    <div className="App">
-     {data.map(item =>{
-      console.log(item)
-     })}
-
-    </div>
-  )
+  return <div className="App">
+    <input type='text' className='something' onChange={e => setChange(e.target.value)}/>
+    <button onClick={() => {handleClick}}>Search</button>
+    
+  </div>
 }
 
 export default App;
