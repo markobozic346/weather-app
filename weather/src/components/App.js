a9d5652bc7c6ac66adb4a9a746ec7c655cee26aa
markobozic346 <55995892+markobozic346@users.noreply.github.com>
2021-03-14 00:24:33 +0100

    added props

--- weather/src/components/App.js
+++ weather/src/components/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
 import React, { useEffect, useState } from "react";
 import Weather from './Weather';
+
 function App() {
+
   //data from api
   const [data, setData] = useState("");
 
@@ -12,28 +14,28 @@ function App() {
   useEffect(() => {
     async function fetchData() {
       let response = await fetch(
-       `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.REACT_APP_API_KEY}`
+       `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${process.env.REACT_APP_API_KEY}`
       );
       let data = await response.json();
       setData(data);
       console.log(data) 
     }
 
-    
+    // if true call fetchData function
      isCityEntered() && fetchData() ;
     
   }, [cityName]);
 
-
-
+  // checks if city is entered
   var isCityEntered = () => (
     (cityName !== "") ? true : false
   )
   
   return <div className="App">
-    <input type='text' className='something' placeholder="enter city name"onChange={e => setChange(e.target.value)}/>
+    <input type='text' className='something' placeholder="Enter City Name"onChange={e => setChange(e.target.value)}/>
     <button onClick={() => {setCityName(change)}}>Get Data</button>
-  {isCityEntered() && <Weather /> }
+    
+  {isCityEntered() && <Weather cityName={data.name} temperatureInfo={data.main} weatherInfo={data.weather}/>}
   </div>
   
 }
--- weather/src/components/Weather.js
+++ weather/src/components/Weather.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
-function Weather() {
+function Weather({cityName, temperatureInfo, weatherInfo}) {
+    
     return (
         <div>
-            <h1>Bla</h1>
+            <h1>{cityName}</h1>
+
+            {/* Checks if temperatureInfo and WeatherInfo are not undefined*/}
+             {(temperatureInfo && weatherInfo) && <> 
+             
+             <h2>{Math.round(temperatureInfo.temp)}</h2>
+             
+             <img src={`http://openweathermap.org/img/w/${weatherInfo[0].icon}.png`}/>
+             
+             </>}
+        
         </div>
     )
 }
