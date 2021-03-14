468b164bcec89cf01527c47535b2a15b3402c42a
markobozic346 <55995892+markobozic346@users.noreply.github.com>
2021-03-14 23:23:37 +0100

    code refactored

--- weather/src/components/App.js
+++ weather/src/components/App.js
@@ -1,45 +1,60 @@
 import "./App.css";
 import React, { useEffect, useState } from "react";
-import Weather from './Weather';
+import Weather from "./Weather";
 
 function App() {
-
   //data from api
   const [data, setData] = useState("");
 
   //data from input field
   const [change, setChange] = useState("");
   const [cityName, setCityName] = useState("");
-  
+
   useEffect(() => {
     async function fetchData() {
       let response = await fetch(
-       `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${process.env.REACT_APP_API_KEY}`
+        `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${process.env.REACT_APP_API_KEY}`
       );
       let data = await response.json();
       setData(data);
-      
     }
 
     // if true call fetchData function
-     isCityEntered() && fetchData() ;
-    
+    isCityEntered() && fetchData();
   }, [cityName]);
 
   // checks if city is entered
-  var isCityEntered = () => (
-    (cityName !== "") ? true : false
-  )
-  
-  return <div className="App">
-    <div className="input-container">
-      <input type='text' className='city-input' placeholder="Enter City Name"onChange={e => setChange(e.target.value)}/>
-      <button className= 'btn-search'onClick={() => {setCityName(change)}}>Get Data</button>
+  var isCityEntered = () => (cityName !== "" ? true : false);
+
+  return (
+    <div className="App">
+      <div className="input-container">
+        <input
+          type="text"
+          className="city-input"
+          placeholder="Enter City Name"
+          onChange={(e) => setChange(e.target.value)}
+        />
+        <button
+          className="btn-search"
+          onClick={() => {
+            setCityName(change);
+          }}
+        >
+          Get Data
+        </button>
+      </div>
+      {/* Renders Weather component only if city is entered */}
+      {isCityEntered() && (
+        <Weather
+          code={data.cod}
+          cityName={data.name}
+          temperatureInfo={data.main}
+          weatherInfo={data.weather}
+        />
+      )}
     </div>
-    {/* Renders Weather component only if city is entered */}
-  {isCityEntered() && <Weather code={data.cod} cityName={data.name} temperatureInfo={data.main} weatherInfo={data.weather}/>}
-  </div>
-  
+  );
 }
 
 export default App;
--- weather/src/components/Weather.js
+++ weather/src/components/Weather.js
@@ -1,27 +1,46 @@
-import React from 'react'
-import './Weather.css'
-function Weather({code, cityName, temperatureInfo, weatherInfo}) {
-    
-    return (
-        // checks if city is found
-        (code === 200) ?
-         <div className="weather-container">
-             
-            {/* Checks if props are not undefined */}
-             {(temperatureInfo && weatherInfo) && <> 
-                
-             <h3 className='city-name'>{cityName}</h3>
-             <h2 className='value'> {Math.round(temperatureInfo.temp)}°</h2>
-             <p className="description">{weatherInfo[0].description}</p>
-             <img className='icon' src={`http://openweathermap.org/img/w/${weatherInfo[0].icon}.png`}/>
-             <p className="feels-like">Feels like: <span className='bigger'>{Math.round(temperatureInfo.feels_like)}°</span></p>
-             <p className="temp-min">min: <span className='bigger'>{Math.round(temperatureInfo.temp_min)}°</span></p>
-             <p className="temp-min">max: <span className='bigger'>{Math.round(temperatureInfo.temp_max)}°</span> </p>
-             
-             </>}
-        {/* if city is not found show: */}
-        </div> : <>City not found</>
+import React from "react";
+import "./Weather.css";
+function Weather({ code, cityName, temperatureInfo, weatherInfo }) {
+
+  const htmlData = () => (
+    <>
+      <h3 className="city-name">{cityName}</h3>
+      <h2 className="value"> {Math.round(temperatureInfo.temp)}°</h2>
+      <p className="description">{weatherInfo[0].description}</p>
+      <img
+        className="icon"
+        src={`http://openweathermap.org/img/w/${weatherInfo[0].icon}.png`}
+      />
+      <p className="feels-like">
+        Feels like:
+        <span className="bigger">
+          {Math.round(temperatureInfo.feels_like)}°
+        </span>
+      </p>
+      <p className="temp-min">
+        min:
+        <span className="bigger">{Math.round(temperatureInfo.temp_min)}°</span>
+      </p>
+      <p className="temp-min">
+        max:
+        <span className="bigger">{Math.round(temperatureInfo.temp_max)}°</span>
+      </p>
+    </>
+  );
+
+  return (
+    // checks if city is found
+    code === 200 ? (
+      <div className="weather-container">
+        {/* if props are not undefined show data*/}
+        {(temperatureInfo && weatherInfo) && htmlData()}
+        
+      </div>
+    ) : (
+        // display if city is not found
+      <>City not found</>
     )
+  );
 }
 
-export default Weather
+export default Weather;
